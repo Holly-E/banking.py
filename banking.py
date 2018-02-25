7c3b3e4dce301e6c8f9260bd2332bdbe5686ba4d
Holly Erickson <Erickson.Holly@icloud.com>
2018-02-25 17:00:41 -0500

    Add two classes and script to add or create account.

---
+++ banking.py
@@ -0,0 +1,19 @@
+class Bank:
+    pass
+
+class Customer:
+    pass
+
+bank = Bank()
+customer = Customer()
+
+while True:
+    print("Enter 1 if you would like to create a new savings account.")
+    print("Enter 2 if you would like to access an existing account.")
+    user_choice = input()
+    if user_choice == 1:
+        pass
+    elif user_choice == 2:
+        pass
+    else:
+        print("Please enter one or two.")
